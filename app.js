71e8d370cb1ed5c8d54ec64cecb8d7b2b9afec75
maomao0007 <skylifepic07@gmail.com>
2024-03-29 14:51:06 +0800

    build: add static files & json for model

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("express app for restaurant list");
+  res.redirect("/Restaurant-List");
 });
 
+app.get("/Restaurant-List", (req, res) => {
+  res.send('listing restaurants');
+});
+
+app.get("/Restaurant-List/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`read restaurant list: ${id}`);
+});
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`);
 });
