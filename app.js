683420dd899ac0311f8098df180535041f686aef
maomao0007 <skylifepic07@gmail.com>
2024-03-30 17:24:43 +0800

    feat: add search function

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const { engine } = require("express-handlebars");
+const restaurants = require("./public/jsons/restaurant.json").results;
+
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
 
 app.use(express.static("public"));
 
@@ -9,12 +15,23 @@ app.get("/", (req, res) => {
 });
 
 app.get("/Restaurant-List", (req, res) => {
-  res.send('listing restaurants');
+  const keyword = req.query.search?.trim();
+  const matchedRestaurants = keyword ? restaurants.filter((shop) =>
+        Object.values(shop).some((property) => {
+          if (typeof property === "string") {
+            return property.toLowerCase().includes(keyword.toLowerCase());
+          }
+          return false;
+        })
+      )
+    : restaurants;
+  res.render("index", { restaurants: matchedRestaurants, keyword: keyword });
 });
 
 app.get("/Restaurant-List/:id", (req, res) => {
   const id = req.params.id;
-  res.send(`read restaurant list: ${id}`);
+  const restaurant = restaurants.find((shop)=> shop.id.toString()===id)
+  res.render('detail',{ restaurant: restaurant });
 });
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`);
